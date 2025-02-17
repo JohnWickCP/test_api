d3849f00a1919b6f3a4bfc3d738a3c11f32d4ea1
JohnWickCP <phonk63utc@gmail.com>
2025-02-17 21:36:45 +0700

    init

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -5,10 +5,11 @@ import com.example.demo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+@CrossOrigin(origins = "http://localhost:9090")
 @Controller
 public class StudentController {
     @Autowired
