0dac184cbb06fbbc55e1628c0841e236e2efcea0
JohnWickCP <phonk63utc@gmail.com>
2025-02-17 21:37:04 +0700

    init

--- src/main/java/com/example/demo/Demo4Application.java
+++ src/main/java/com/example/demo/Demo4Application.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo4Application {
+    private static final Logger logger = LoggerFactory.getLogger(Demo4Application.class);
 
     public static void main(String[] args) {
+        logger.info("Starting Demo4Application...");
         SpringApplication.run(Demo4Application.class, args);
+        logger.info("Demo4Application started successfully.");
     }
-
 }
